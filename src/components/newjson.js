524b911f6846606971c2441805ae170f7ce32a48
yusuf0174 <shahriarfrances@gmail.com>
2021-09-27 17:40:35 +0600

    first commit

---
+++ src/components/newjson.js
@@ -0,0 +1,111 @@
+const companies = [
+    {
+        id: '0001',
+        name: 'Apple Inc.',
+        about: 'Apple is an American multinational technology company that specializes in consumer electronics, computer software, and online services.',
+        ceo: 'Tim Cook',
+        location: 'United States',
+        established: '1976',
+        capital: 2100,
+        img: 'https://images.unsplash.com/photo-1598972428564-8dbf76afc2ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2969&q=80'
+    },
+    
+    {
+        id: '0002',
+        name: 'Google Inc.',
+        about: 'Google is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine.',
+        ceo: 'Sundar Pichai',
+        location: 'United States',
+        established: '1998',
+        capital: 1897,
+        img: 'https://images.unsplash.com/photo-1600367163359-d51d40bcb5f8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3086&q=80'
+    },
+
+    {
+        id: '0003',
+        name: 'Microsoft Corporation',
+        about: 'Microsoft is an American multinational technology corporation which produces computer software, consumer electronics, personal computers, and related services.',
+        ceo: 'Bill Gates',
+        location: 'United States',
+        established: '1975',
+        capital: 1800,
+        img: 'https://cdn.wallpapersafari.com/15/49/rCpq2H.jpg'
+    },
+
+    {
+        id: '0004',
+        name: 'Amazon.com, Inc.',
+        about: 'Amazon is an American multinational conglomerate which focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence.',
+        ceo: 'Jeff Bezos',
+        location: 'United States',
+        established: '1994',
+        capital: 1600,
+        img: 'https://economictimes.indiatimes.com/thumb/msid-81479140,width-1200,height-900,resizemode-4,imgsize-351750/untitled-1.jpg?from=mdr'
+    },
+
+    {
+        id: '0005',
+        name: 'Facebook, Inc.',
+        about: 'Facebook is an American multinational technology company based in Menlo Park, California. It was founded in 2004 as TheFacebook by Mark Zuckerberg.',
+        ceo: 'Mark Zuckerberg',
+        location: 'United States',
+        established: '2004',
+        capital: 839,
+        img: 'https://i.insider.com/5d9f5bd73b33a74aa94e9913?width=2000&format=jpeg&auto=webp'
+    },
+
+    {
+        id: '0006',
+        name: 'Tesla, Inc.',
+        about: 'Tesla is an American electric vehicle and clean energy company based in Palo Alto, California, United States.',
+        ceo: 'Elon Musk',
+        location: 'United States',
+        established: '2003',
+        capital: 641,
+        img: 'https://blog.logomyway.com/wp-content/uploads/2019/06/tesla-logo-design.jpg'
+    },
+
+    {
+        id: '0007',
+        name: 'PayPal',
+        about: 'PayPal is an American multinational financial technology company operating an online payments system in the majority of countries that support online money transfers.',
+        ceo: 'Dan Schulman',
+        location: 'United States',
+        established: '1998',
+        capital: 284,
+        img: 'https://static01.nyt.com/images/2017/03/01/business/02xp-paypal/02xp-paypal-superJumbo.jpg?quality=90&auto=webp'
+    },
+
+    {
+        id: '0008',
+        name: 'Intel Corporation',
+        about: 'Intel is an American multinational corporation and technology company headquartered in Santa Clara, California.',
+        ceo: 'Patrick Gelsinger',
+        location: 'United States',
+        established: '1968',
+        capital: 261,
+        img: 'https://cdn.wccftech.com/wp-content/uploads/2015/09/Intel-Skylake-Processors.jpg'
+    },
+
+    {
+        id: '0009',
+        name: 'Toyota Motor Corporation',
+        about: 'Toyota is a Japanese multinational automotive manufacturer headquartered in Toyota City, Aichi, Japan.  It was founded by Kiichiro Toyoda.',
+        ceo: 'Akio Toyoda',
+        location: 'Japan',
+        established: '1937',
+        capital: 254,
+        img: 'https://logos-world.net/wp-content/uploads/2020/04/Toyota-Logo-1989-present.jpg'
+    },
+
+    {
+        id: '0010',
+        name: 'Netflix, Inc.',
+        about: 'Netflix is an American over-the-top media service and original programming production company. It offers subscription-based video on demand from a library of films and television series.',
+        ceo: 'Ted Sarandos',
+        location: 'United States',
+        established: '1997',
+        capital: 231,
+        img: 'https://i1.wp.com/hive-store.com/wp-content/uploads/2020/03/netflix.png?fit=2950%2C1554&ssl=1'
+    }
+];
